af2e25cf874d84dbefd91060815e89423b332cf2
Sumanth <va20070770@wipro.com>
2019-06-22 14:21:06 +0530

    package change

--- src/main/java/com/jms/demo/sender/ClientController.java
+++ src/main/java/com/jms/demo/sender/ClientController.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.jms.demo;
+package com.jms.demo.sender;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
